1191b5b960772cf43ec3a62ec204585b8c6d7415
Juan David Medina Tobon <jdmedinatobon@gmail.com>
2019-05-09 15:28:09 -0500

    scripts prueba motores

--- Scripts/leviathan.py
+++ Scripts/leviathan.py
@@ -3,14 +3,29 @@
 import rospy
 import RPi.GPIO as GPIO
 
-#Es la tasa en Hertz del nodo.
+#Es la tasa en Hertz (Hz) del nodo.
 h = 10
+
+#Variable con el pin que va al motor.
+pwm1 = 10
+
+#Frecuencia en Hertz (Hz) del pin que va al motor.
+f1 = 500
+
+#Ciclo útil del pulso. Un número entre 0 y 100.
+ciclo = 1
+
+def prender():
+    p.start(ciclo)
 
 def leviathan():
 
     rate = rospy.Rate(h)
+    p = GPIO.PWM(pwm1,f1)
+    p.start(0)
 
     while not rospy.is_shutdown():
+        prender()
         rate.sleep()
 
 if __name__ == '__main__':
