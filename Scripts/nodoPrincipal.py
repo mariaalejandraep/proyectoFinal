2f6b0e78c9433f29d0844a0e9de5b6b362c31256
Juan David <jd.serranom@uniandes.edu.co>
2019-05-25 23:20:01 -0500

    c

--- Scripts/nodoPrincipal.py
+++ Scripts/nodoPrincipal.py
@@ -35,7 +35,7 @@ def leviathan():
     pubEstado.publish(0)  # Aca publica que esta esperando ack_service 0
     rospy.wait_for_service('ack_service')  # Espera a que se cree el servicio
     rospy.loginfo("Esperando ack_service")
-    ack_service = rospy.ServiceProxy('ack_service', Int32)  # Crea el objeto referente al servicio
+    ack_service = rospy.ServiceProxy('ack_service', AckService)  # Crea el objeto referente al servicio
     ip = String()
     ip.data = socket.gethostbyname(socket.gethostname())  # identifica la ip del dispositivo
     groupNumber = Int32()
@@ -44,7 +44,7 @@ def leviathan():
     resp.data = 0
     try:
         while resp.data == 0:
-            resp = ack_service(groupNumber, ip)  # Solicita la repsuesta del servicio
+            resp = ack_service(groupNumber, ip)  # Solicita la respuesta del servicio
             if resp.data != 1:
                 resp.data = 0
             time.sleep(1)
