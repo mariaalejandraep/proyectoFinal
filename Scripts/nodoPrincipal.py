f2f2d40a6f178008540637cae9a2e5bb180866c4
Juan David <jd.serranom@uniandes.edu.co>
2019-05-25 20:45:47 -0500

    A

--- Scripts/nodoPrincipal.py
+++ Scripts/nodoPrincipal.py
@@ -7,7 +7,7 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Pose
 from master_msgs_iele3338.msg import Obstacle
 from master_msgs_iele3338.srv import StartService, EndService
-from proyectoFinal.srv import Contrasena
+from srv import Contrasena
 import TerminarServicio
 
 # Posicion de inicio del robot
