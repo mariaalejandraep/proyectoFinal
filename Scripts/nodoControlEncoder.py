ff9e46b3ebedd14728b926736282e5fef363aa85
Juan David <jd.serranom@uniandes.edu.co>
2019-05-28 10:56:48 -0500

    hola

--- Scripts/nodoControlEncoder.py
+++ Scripts/nodoControlEncoder.py
@@ -36,8 +36,8 @@ pA2 = None
 pB1 = None
 pB2 = None
 # Variables de conteo de flancos de encoders
-subidaA1 = []
-subidaB1 = []
+contadorA = 0
+
 subidaA2 = []
 subidaB2 = []
 # Variable booleana  que inidca si se esta calculando velocidad
@@ -88,10 +88,10 @@ def setPins():
     pB2 = GPIO.PWM(pwmB2Driver, fDriver)
     GPIO.output(pwmB2Driver, 0)
     # Detectar flancos en otros metodos
-    GPIO.add_event_detect(pwmA1Encoder, GPIO.RISING, callback=sumarFlancoA1)
-    GPIO.add_event_detect(pwmB1Encoder, GPIO.RISING, callback=sumarFlancoB1)
-    GPIO.add_event_detect(pwmA2Encoder, GPIO.RISING, callback=sumarFlancoA2)
-    GPIO.add_event_detect(pwmB2Encoder, GPIO.RISING, callback=sumarFlancoB2)
+    GPIO.add_event_detect(pwmA1Encoder, GPIO.BOTH, callback=sumarFlancoA1)
+    GPIO.add_event_detect(pwmB1Encoder, GPIO.BOTH, callback=sumarFlancoB1)
+    # GPIO.add_event_detect(pwmA2Encoder, GPIO.RISING, callback=sumarFlancoA2)
+    # GPIO.add_event_detect(pwmB2Encoder, GPIO.RISING, callback=sumarFlancoB2)
 
 
 def controlBajoNivel():
@@ -102,32 +102,33 @@ def controlBajoNivel():
     pA1.ChangeDutyCycle(cicloADriver)
     pB1.ChangeDutyCycle(cicloBDriver)
     while not rospy.is_shutdown():
-        calcularVelocidadRuedas()
-        aplicarControlBajoNivel()
+        # calcularVelocidadRuedas()
+        # aplicarControlBajoNivel()
+        print(contadorA)
         rate.sleep()
     apagar()
 
 
-def calcularVelocidadRuedas():
-    global velActA, velActB, movingA, movingB
-    if movingA:
-        if subidaA1[len(subidaA1) - 1] < subidaB1[len(subidaB1) - 1]:
-            velActA = -1 * (2 * math.pi() * r) / (subidaA1[len(subidaA1) - 1] - subidaA1[0])
-        else:
-            velActA = (2 * math.pi() * r) / (subidaB1[len(subidaB1) - 1] - subidaB1[0])
-        movingA = False
-    else:
-        velActA = 0
-    if movingB:
-        if subidaA2[len(subidaA2) - 1] < subidaB2[len(subidaB2) - 1]:
-            velActB = (2 * math.pi() * r) / (subidaA2[len(subidaA2) - 1] - subidaA2[0])
-        else:
-            velActB = -1 * (2 * math.p() * r) / (subidaB2[len(subidaB2) - 1] - subidaB2[0])
-        movingB = False
-    else:
-        velActB = 0
-    print("La velocidad actual de la rueda A:", velActA)
-    print("La velocidad actual de la rueda B:", velActB)
+# def calcularVelocidadRuedas():
+#     global velActA, velActB, movingA, movingB
+#     if movingA:
+#         if subidaA1[len(subidaA1) - 1] < subidaB1[len(subidaB1) - 1]:
+#             velActA = -1 * (2 * math.pi() * r) / (subidaA1[len(subidaA1) - 1] - subidaA1[0])
+#         else:
+#             velActA = (2 * math.pi() * r) / (subidaB1[len(subidaB1) - 1] - subidaB1[0])
+#         movingA = False
+#     else:
+#         velActA = 0
+#     if movingB:
+#         if subidaA2[len(subidaA2) - 1] < subidaB2[len(subidaB2) - 1]:
+#             velActB = (2 * math.pi() * r) / (subidaA2[len(subidaA2) - 1] - subidaA2[0])
+#         else:
+#             velActB = -1 * (2 * math.p() * r) / (subidaB2[len(subidaB2) - 1] - subidaB2[0])
+#         movingB = False
+#     else:
+#         velActB = 0
+#     print("La velocidad actual de la rueda A:", velActA)
+#     print("La velocidad actual de la rueda B:", velActB)
 
 
 def aplicarControlBajoNivel():
@@ -197,18 +198,13 @@ def apagar():
 
 
 def sumarFlancoA1(a):
-    global subidaA1, moving
-    subidaA1.append(time.time())
-    subidaA1 = subidaA1[-13:]
-
-    moving = True
+    global contadorA
+    contadorA = contadorA + 1
 
 
 def sumarFlancoB1(a):
-    global subidaB1, moving
-    subidaB1.append(time.time())
-    subidaB1 = subidaB1[-13:]
-    moving = True
+    global contadorA
+    contadorA = contadorA + 1
 
 
 def sumarFlancoA2(a):
