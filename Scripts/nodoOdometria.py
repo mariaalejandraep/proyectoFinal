fb53c6152c8bdb891c7d21b09f35c5b45cb15453
Juan David <jd.serranom@uniandes.edu.co>
2019-05-29 12:55:29 -0500

    A

--- Scripts/nodoOdometria.py
+++ Scripts/nodoOdometria.py
@@ -108,19 +108,23 @@ def actualizar(msg):
     while pos.orientation.w > math.pi:
         pos.orientation.w = pos.orientation.w - 2*math.pi
 
-    CovarSrSl = np.array(((kr*np.absolute(dSr), 0), (0, kl*np.absolute(dSl))))
+    CovarSrSl = np.matrix(((kr*np.absolute(dSr), 0), (0, kl*np.absolute(dSl))))
+    rospy.loginfo ("CovarSrsl")
     rospy.loginfo (CovarSrSl)
-    Fpt1=np.array([[1, 0, -dSsin],[1, 0, dScos],[0, 0, 1]])
+    Fpt1=np.matrix([[1, 0, -dSsin],[1, 0, dScos],[0, 0, 1]])
+    rospy.loginfo ("Fpt1")
     rospy.loginfo (Fpt1)
     Fpt1trans=np.transpose(Fpt1)
+    rospy.loginfo ("Fpt1trans")
     rospy.loginfo (Fpt1trans)
-    FdS=np.array([[(1/2)*cose-(1/(2*b))*dSsin,(1/2)*cose+(1/(2*b))*dSsin],[(1/2)*seno+(1/(2*b))*dScos,(1/2)*seno-(1/(2*b))*dScos],[1/b,-(1/b)]])
+    FdS=np.matrix([[(1/2)*cose-(1/(2*b))*dSsin,(1/2)*cose+(1/(2*b))*dSsin],[(1/2)*seno+(1/(2*b))*dScos,(1/2)*seno-(1/(2*b))*dScos],[1/b,-(1/b)]])
+    rospy.loginfo ("FdS")
     rospy.loginfo (FdS)
     FdStrans=np.transpose(FdS)
+    rospy.loginfo ("FdStrans")
     rospy.loginfo (FdStrans)
 
     Covarianza=(Fpt1.dot(Covarianza)).dot(Fpt1trans) + (FdS.dot(CovarSrSl)).dot(FdStrans)
-    rospy.loginfo (Covarianza)
 
     cov.sigma11 = Covarianza[0, 0]
     cov.sigma12 = Covarianza[0, 1]
@@ -131,6 +135,7 @@ def actualizar(msg):
     cov.sigma31 = Covarianza[2, 0]
     cov.sigma32 = Covarianza[2, 1]
     cov.sigma33 = Covarianza[2, 2]
+    rospy.loginfo ("cov")
     rospy.loginfo(cov)
 
     pubPos.publish(pos)
