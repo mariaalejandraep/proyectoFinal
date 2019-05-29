ca857efb558e0461a5c4f7a7db7e2273335537f1
Juan David <jd.serranom@uniandes.edu.co>
2019-05-28 23:16:48 -0500

    M

--- Scripts/nodoControlEncoder.py
+++ Scripts/nodoControlEncoder.py
@@ -57,11 +57,11 @@ calculando = False
 r = (29.3/2)
 # Variables de control PI
 kpA = 0.08
-kiA = 0.00000001
-kdA = 0.001
+kiA = 0#0.00000001
+kdA = 0#0.001
 kpB = 0.08
-kiB = 0.00000001
-kdB = 0.001
+kiB = 0#0.00000001
+kdB = 0#0.001
 # Acumulacion de error para integrador
 integradorA = []
 integradorB = []
